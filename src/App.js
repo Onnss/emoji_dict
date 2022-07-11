897d35d996b1ff203fe9d8dc91bda27ba4bdded4
Onnss <62172462+Onnss@users.noreply.github.com>
2022-07-11 15:03:26 +0100

    'v1.1'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import emojiss from './component/emojis';
 import Card from './component/Card';
-function emoj(emo){
-return (<Card
-  key={emo.id}
-  emoji={emo.emoji}
-  name={emo.name}
-  description={emo.meaning}
-/>);
-}
+
 function App() {
   return (
     <div className="App d-flex row justify-content-center">
       <h1 className='fw-bold fs-1 text-white mt-5'>Emojipedia</h1>
-      {emojiss.map(emoj)}
+      {emojiss.map(emo=>(<Card key={emo.id} emoji={emo.emoji} name={emo.name} description={emo.meaning} />))}
     </div>
   );
 }
